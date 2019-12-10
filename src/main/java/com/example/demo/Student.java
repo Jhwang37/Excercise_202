8b74609bd9f8428b54aeef4c5167a78fbc9f2dcc
jhwang37 <jhwang37@gmail.com>
2019-12-10 09:28:39 -0500

    first commit

---
+++ src/main/java/com/example/demo/Student.java
@@ -0,0 +1,58 @@
+package com.example.demo;
+
+public class Student {
+        private String name;
+        private String schoolName;
+        private String email;
+        private int age;
+        private int height;
+        private int phoneNumber;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSchoolName() {
+        return schoolName;
+    }
+
+    public void setSchoolName(String schoolName) {
+        this.schoolName = schoolName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
